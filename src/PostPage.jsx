4ae0a91460d1671f2a479fc5398a17d40b06cec1
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-01-18 18:05:03 +0100

    Everything working fine now

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -3,7 +3,7 @@ import { useParams, Link } from "react-router-dom";
 
 const PostPage = ({ posts, handleDelete }) => {
   const { id } = useParams();
-  const post = posts.find((post) => (post.id).toString() === id);
+  const post = posts.find((post) => post.id.toString() === id);
   return (
     <main className="postPage">
       <article className="post">
@@ -12,18 +12,21 @@ const PostPage = ({ posts, handleDelete }) => {
             <h2>{post.title}</h2>
             <p className="postDate">{post.datetime}</p>
             <p className="postBody">{post.body}</p>
-            <button onClick={() => handleDelete(post.id)}>Delete Post</button>
+            <Link to={`/edit/${post.id}`}>
+              <button className="editButton">Edit Post</button>
+            </Link>
+            <button className="deleteButton" onClick={() => handleDelete(post.id)}>Delete Post</button>
           </>
         )}
-        {!post &&
-        <>
-          <h2>Post Not Found</h2>
-          <p>Well, that's disappointing</p>
-          <p>
-            <Link to="/">Visit our Homepage</Link>
-          </p>
-        </>
-        }
+        {!post && (
+          <>
+            <h2>Post Not Found</h2>
+            <p>Well, that's disappointing</p>
+            <p>
+              <Link to="/">Visit our Homepage</Link>
+            </p>
+          </>
+        )}
       </article>
     </main>
   );
